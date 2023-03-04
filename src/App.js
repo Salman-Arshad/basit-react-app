8253ba0e0a8e3e42bbc1be4a8fccadb6bbbcafb0
Salman Arshad <salman@koii.network>
2023-03-04 18:02:23 +0500

    changed named to ali

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with basit
+          Learn React with salman
         </a>
       </header>
     </div>
