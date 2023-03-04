f86adba9466d48ba9211217d286ed3e4cc7cef69
Salman Arshad <salman@koii.network>
2023-03-04 18:01:45 +0500

    changed named to saad

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with basit
+          Learn React with saad
         </a>
       </header>
     </div>
